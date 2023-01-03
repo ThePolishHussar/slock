983851a660fb86862f7f10e66e730c3cdca24029
Jakub <jakubb.06@protonmail.com>
2023-01-03 11:41:53 -0500

    add/change some xresources defentitions

--- config.def.h
+++ config.def.h
@@ -10,16 +10,6 @@ static const char *colorname[NUMCOLS] = {
 	[CAPS] = "yellow",         /* CapsLock on */
 };
 
-/*
- * Xresources preferences to load at startup
- */
-ResourcePref resources[] = {
-		{ "color0",       STRING,  &colorname[INIT] },
-		{ "color4",       STRING,  &colorname[INPUT] },
-		{ "color1",       STRING,  &colorname[FAILED] },
-		{ "color3",       STRING,  &colorname[CAPS] },
-};
-
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
 
@@ -42,8 +32,19 @@ static XRectangle rectangles[9] = {
 /*Enable blur*/
 #define BLUR
 /*Set blur radius*/
-static const int blurRadius=5;
+static int blurRadius=5;
 /*Enable Pixelation*/
 //#define PIXELATION
 /*Set pixelation radius*/
 static const int pixelSize=0;
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "initcolor",	STRING,		&colorname[INIT] },
+		{ "inputcolor",	STRING,		&colorname[INPUT] },
+		{ "failcolor",	STRING,		&colorname[FAILED] },
+		{ "capscolor",	STRING,		&colorname[CAPS] },
+		{ "blurradius", INTEGER,	&blurRadius },
+};
